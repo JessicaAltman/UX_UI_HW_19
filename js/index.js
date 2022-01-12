1fdfa6df3ce1c0d19e13ad73ae9674a3d06fdd7a
Jessica <waite.jessica@comcast.net>
2022-01-11 23:30:52 -0800

    CSS
    Adding background/color to page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("#toggle").on("click", function(){
+    $("p").slideToggle();
+    $("./images/Icon ionic-ios-arrow-down.png").slideToggle()
+})
+
+$("#toggle").on("click", function(){
+    $("p").slideToggle("webDevelopment");
+    $("./images/Icon ionic-ios-arrow-down.png").slideToggle()
+})
